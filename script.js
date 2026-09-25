2a9b4f1b4bb5c4c43f1dc042fc1a9ba02c6fdb30
gumacorporation <alexgumo203+github@gmail.com>
2026-09-24 22:42:28 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card, i) => {
+        card.style.opacity = "0";
+        card.style.transform = "translateY(20px)";
+        setTimeout(() => {
+            card.style.transition = "0.6s";
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }, 200 * i);
+    });
+});
